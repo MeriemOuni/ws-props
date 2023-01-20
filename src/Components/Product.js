18d07292733f79d874a9c86d29de766dcfa0b15f
meriem <elounimeriem@gmail.com>
2023-01-20 09:44:51 +0100

    second commit

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,22 +1,33 @@
-import React from 'react'
-import {Card, Button} from 'react-bootstrap'
+import React from "react";
+import { Card, Button } from "react-bootstrap";
+import PropTypes from "prop-types";
 
 const Product = (props) => {
-    const handleName = (mark) => alert(`The product mark is ${props.mark}`)
-    // console.log(props)
-    return (
-        <div style={{display:'flex'}}>
-            <Card style={{ width: '18rem' }}>
-                {props.children}
-                <Card.Body>
-                    <Card.Title>{props.name}</Card.Title>
-                    <Card.Text>{props.price}</Card.Text>
-                    <Card.Text>{props.mark}</Card.Text>
-                    <Button variant="primary" onClick={handleName}>Click Me</Button>
-                </Card.Body>
-            </Card>
-        </div>
-    )
-}
+	const handleName = (mark) => alert(`The product name is ${props.mark}`);
+	// console.log(props)
+	return (
+		<div style={{ display: "flex" }}>
+			<Card style={{ width: "18rem" }}>
+				{props.children}
+				<Card.Body>
+					<Card.Title>{props.name}</Card.Title>
+					<Card.Text>{props.price}</Card.Text>
+					<Card.Text>{props.mark}</Card.Text>
+					<Button variant='primary' onClick={handleName}>
+						Click Me
+					</Button>
+				</Card.Body>
+			</Card>
+		</div>
+	);
+};
 
-export default Product
+Product.defaultProps = {
+	title: "this is e title",
+};
+
+Product.propTypes = {
+	age: PropTypes.number,
+};
+
+export default Product;
